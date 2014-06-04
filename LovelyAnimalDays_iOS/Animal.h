450261abf10349bab98f09e769acacb67753915b
tsuchikazu <huneyhunt77@gmail.com>
2014-06-05 00:18:33 +0900

    Animalクラスを新規追加

---
+++ LovelyAnimalDays_iOS/Animal.h
@@ -0,0 +1,13 @@
+//
+//  Animal.h
+//  LovelyAnimalDays_iOS
+//
+//  Created by 土屋 和良 on 2014/06/05.
+//  Copyright (c) 2014年 土屋 和良. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Animal : NSObject
+
+@end
---
+++ LovelyAnimalDays_iOS/Animal.m
@@ -0,0 +1,13 @@
+//
+//  Animal.m
+//  LovelyAnimalDays_iOS
+//
+//  Created by 土屋 和良 on 2014/06/05.
+//  Copyright (c) 2014年 土屋 和良. All rights reserved.
+//
+
+#import "Animal.h"
+
+@implementation Animal
+
+@end
